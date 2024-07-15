745dce8cd5c867b58754235cd403176bf3cdc1ce
Haru <23bus1maesub@gmail.com>
2024-07-15 18:32:20 +0900

    新規: starlight導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,35 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight'
+      },
+      sidebar: [{
+        label: 'Guides',
+        items: [
+        // Each item here is one entry in the navigation menu.
+        {
+          label: 'Example Guide',
+          slug: 'guides/example'
+        }]
+      }, {
+        label: 'Reference',
+        autogenerate: {
+          directory: 'reference'
+        }
+      }],
+      customCss: [
+        './src/tailwind.css',
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ]
 });
