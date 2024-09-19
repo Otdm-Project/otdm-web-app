814786b3cd58572d950d384f71742b22d988d3f3
Haru-0001 <23bus1maesub@gmail.com>
2024-09-20 00:36:38 +0900

    改善: ヘッダーの改善

--- src/components/style.ts
+++ src/components/style.ts
@@ -1,12 +1,12 @@
 import { tv } from 'tailwind-variants';
 
 export const otdmButton = tv({
-    base: 'flex justify-center items-center size-12 rounded-lg',
+    base: 'flex justify-center items-center size-14 rounded-lg',
     variants: {
         color:{
             docs: 'text-orange-900 bg-orange-400',
             download: 'text-yellow-900 bg-yellow-300',
-            git: 'text-black bg-sky-100',
+            git: 'text-black bg-slate-100',
             darkMode: 'text-yellow-100 bg-sky-900',
             lightMode: 'text-orange-300 bg-sky-100',
         }
