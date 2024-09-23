7416eabf99ac338c033aac8ee22086de91109a83
Haru-0001 <23bus1maesub@gmail.com>
2024-09-24 01:22:29 +0900

    改善: header及びhooterの改善

--- src/components/style.ts
+++ src/components/style.ts
@@ -4,10 +4,10 @@ export const otdmButton = tv({
     base: 'flex justify-center items-center size-14 rounded-lg',
     variants: {
         color:{
-            docs: 'text-orange-900 bg-orange-400',
-            download: 'text-yellow-900 bg-yellow-300',
-            git: 'text-black bg-slate-100',
-            darkMode: 'text-yellow-100 bg-sky-900',
+            docs: 'text-orange-900 bg-orange-400 hover:bg-orange-500',
+            download: 'text-yellow-900 bg-yellow-300 hover:bg-yellow-400',
+            git: 'text-black bg-slate-100 hover:bg-slate-300',
+            darkMode: 'text-yellow-100 bg-sky-900 hover:bg-sky-950',
             lightMode: 'text-orange-300 bg-sky-100',
         }
     }
