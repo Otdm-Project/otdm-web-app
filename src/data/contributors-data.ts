a13af827cac539c7cf965367145f8d9a93085878
Haru-0001 <23bus1maesub@gmail.com>
2024-10-01 02:50:35 +0900

    改善: コントリビューターのデータ取得方式を改善

---
+++ src/data/contributors-data.ts
@@ -0,0 +1,33 @@
+interface ContributorType {
+  name: string;
+  avatar: string;
+  github: string;
+}
+
+export const contributors: ContributorType[] = [
+  {
+    name: 'ODENKITUNE',
+    github: 'https://github.com/ODENKITUNE',
+    avatar: 'https://avatars.githubusercontent.com/u/88931859',
+  },
+  {
+    name: 'Yayuki Kazuha',
+    github: 'https://github.com/Yayuki-Kazuha',
+    avatar: 'https://avatars.githubusercontent.com/u/108244620',
+  },
+  {
+    name: 'Haru',
+    github: 'https://github.com/Haru-0001',
+    avatar: 'https://avatars.githubusercontent.com/u/166319111',
+  },
+  {
+    name: 'run',
+    github: 'https://github.com/RUN52114',
+    avatar: 'https://avatars.githubusercontent.com/u/168060328',
+  },
+  {
+    name: 'maritos',
+    github: 'https://github.com/maritos-net',
+    avatar: 'https://avatars.githubusercontent.com/u/168062362',
+  }
+]
