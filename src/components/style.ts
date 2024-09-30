f6b8e50005e8cc8316bf688604f5f86c570dcd3d
Haru-0001 <23bus1maesub@gmail.com>
2024-09-30 23:03:40 +0900

    改善: インデントのスペース数を変更

--- src/components/style.ts
+++ src/components/style.ts
@@ -1,14 +1,14 @@
 import { tv } from 'tailwind-variants';
 
 export const otdmButton = tv({
-    base: 'flex justify-center items-center size-12 rounded-lg',
-    variants: {
-        color:{
-            docs: 'text-orange-900 bg-orange-400 hover:bg-orange-500',
-            download: 'text-yellow-900 bg-yellow-300 hover:bg-yellow-400',
-            git: 'text-black bg-slate-100 hover:bg-slate-300',
-            darkMode: 'text-yellow-100 bg-sky-900 hover:bg-sky-950',
-            lightMode: 'text-orange-300 bg-sky-100',
-        }
+  base: 'flex justify-center items-center size-12 rounded-lg',
+  variants: {
+    color:{
+      docs: 'text-orange-900 bg-orange-400 hover:bg-orange-500',
+      download: 'text-yellow-900 bg-yellow-300 hover:bg-yellow-400',
+      git: 'text-black bg-slate-100 hover:bg-slate-300',
+      darkMode: 'text-yellow-100 bg-sky-900 hover:bg-sky-950',
+      lightMode: 'text-orange-300 bg-sky-100',
     }
+  }
 })
