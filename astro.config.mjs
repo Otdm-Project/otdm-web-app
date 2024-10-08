5fe6aa031869999c66a247a110d700336eb9bc48
Haru <23bus1maesub@gmail.com>
2024-10-08 14:36:40 +0900

    改善: tailwindCSSからunoCSSに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import UnoCSS from '@unocss/astro'
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
+    UnoCSS({
+      injectReset: true
+    }),
     icon()
   ]
 });
