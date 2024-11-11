94500dc38c8a60fbb2b3141a2f354cc8a2afec5c
Haru <23bus1maesub@gmail.com>
2024-11-11 10:30:04 +0900

    astroアップデート&SolidJSを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from '@unocss/astro'
 
+import solid from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    UnoCSS({
-      injectReset: true
-    })
+    UnoCSS({injectReset: true}),
+    solid()
   ],
   image: {
     domains: ["github.com"],
