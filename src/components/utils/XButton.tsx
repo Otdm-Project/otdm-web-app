7271ae1706cba04e7577810a459e94614afb5299
Haru <23bus1maesub@gmail.com>
2024-11-28 12:07:36 +0900

    修正: 動的クラスがUNOCSSで読み込めなっかったため修正

--- src/components/utils/XButton.tsx
+++ src/components/utils/XButton.tsx
@@ -1,16 +1,12 @@
 export function XButton(props: {
   onClick: () => void;
-  size?: number;
-  position?: string;
 }) {
-  const size = props.size ?? 8;
-  const position = props.position ?? "top-3 right-3";
   return (
     <button 
-      class={`absolute ${position} p-1 rounded-lg hover:bg-docs-hover`}
+      class="absolute top-3 right-3 p-1 rounded-lg hover:bg-docs-hover"
       onClick={props.onClick} 
       >
-      <div class={`i-tabler:x size-${size} text-cyan-950`}/>
+      <div class="i-tabler:x size-8 sm:size-6 text-cyan-950"/>
     </button>
   )
 }
