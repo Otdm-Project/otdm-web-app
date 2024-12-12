3ee3ac786f8ed63d8ee0ae2baffb1a3b3f0bb1a3
Haru <23bus1maesub@gmail.com>
2024-12-12 12:46:49 +0900

    修正: OtdmButtonが表示されない問題を修正

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -2,29 +2,21 @@ type navLinksType = {
   name: string;
   href: string;
   blank?: boolean;
-  style?: string;
-  icon?: string;
 };
 
 export const navLinks: Record<string, navLinksType> = {
   docs : {
     name : "ドキュメント",
     href : "/docs",
-    style : "btn-docs",
-    icon : "i-tabler:notes",
   },
   download : {
     name : "ダウンロード",
     href : "/download",
-    style : "btn-download",
-    icon : "i-tabler:download",
   },
   github : {
     name : "GitHub",
     href : "https://github.com/Otdm-Project",
     blank : true,
-    style : "btn-github",
-    icon : "i-custom:github",
   },
   privacy : {
     name : "プライバシーポリシー",
