b15185c753f6c28c7e3fc6af633071d9b8046718
Haru <23bus1maesub@gmail.com>
2025-01-07 16:42:52 +0900

    修正: モバイル版のヘッダーアイコンの中央揃えがされていない問題の修正

--- src/components/overlay/menu/MenuButton.tsx
+++ src/components/overlay/menu/MenuButton.tsx
@@ -3,18 +3,18 @@ import { toggleMenu, isMenuOpen, isClosing } from "../overlay";
 export function MenuButton() {
   return (
     <button class="btn-menu" onClick={toggleMenu}>
-    <div class="relative h-1em w-1em overflow-hidden">
-      <div class={`absolute property-all duration-300 ease-in-out ${
-        isMenuOpen() && !isClosing() ? '-translate-x-full opacity-0 invisible' : 'translate-x-0'
-        }`}>
-        <div class="i-tabler:menu-2 size-1em" />
-      </div>
-      <div class={`absolute property-all duration-300 ease-in-out ${
-        isMenuOpen() && !isClosing() ? 'translate-x-0' : 'translate-x-full opacity-0 invisible'
-        }`}>
+      <div class="relative h-1em w-1em overflow-hidden">
+        <div class={`absolute property-all duration-300 ease-in-out ${
+          isMenuOpen() && !isClosing() ? '-translate-x-full opacity-0 invisible' : 'translate-x-0'
+          }`}>
+          <div class="i-tabler:menu-2 size-1em" />
+        </div>
+        <div class={`absolute property-all duration-300 ease-in-out ${
+          isMenuOpen() && !isClosing() ? 'translate-x-0' : 'translate-x-full opacity-0 invisible'
+          }`}>
         <div class="i-tabler:x size-1em" />
+        </div>
       </div>
-    </div>
-  </button>
+    </button>
   )
 }
