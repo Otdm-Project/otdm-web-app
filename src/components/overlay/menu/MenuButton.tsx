4b4cf650a8e0104a8ddfe0dfe26e446126a8e3cb
Haru-0001 <23bus1maesub@gmail.com>
2025-01-29 22:33:02 +0900

    修正: モバイル版のshadowを削除

--- src/components/overlay/menu/MenuButton.tsx
+++ src/components/overlay/menu/MenuButton.tsx
@@ -2,13 +2,13 @@ import { toggleMenu, isMenuOpen, isClosing } from "../overlay";
 
 export function MenuButton() {
   return (
-    <button class="relative btn-menu shadow-md size-otdm-btn overflow-hidden" onClick={toggleMenu}>
-      <div class={`absolute inset-0 m-auto  grid place-content-center property-all duration-300 ease-in-out ${
+    <button class="relative btn-menu sm:shadow-md size-otdm-btn overflow-hidden" onClick={toggleMenu}>
+      <div class={`absolute inset-0 grid place-content-center property-all duration-300 ease-in-out ${
         isMenuOpen() && !isClosing() ? '-translate-x-full opacity-0 invisible' : 'translate-x-0'
         }`}>
         <div class="i-tabler:menu-2 size-otdm-icon" />
       </div>
-      <div class={`absolute inset-0 m-auto  grid place-content-center property-all duration-300 ease-in-out ${
+      <div class={`absolute inset-0 grid place-content-center property-all duration-300 ease-in-out ${
         isMenuOpen() && !isClosing() ? 'translate-x-0' : 'translate-x-full opacity-0 invisible'
         }`}>
         <div class="i-tabler:x size-otdm-icon" />
