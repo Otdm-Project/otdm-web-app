69068e87be83468a5baaa395eb6ecca2c80c0e1e
Haru <23bus1maesub@gmail.com>
2025-01-30 11:05:47 +0900

    修正: memberの名前を修正

--- src/data/members.ts
+++ src/data/members.ts
@@ -38,8 +38,8 @@ export const members: MembersType[] = [
     bio: 'ネットワークに興味が出てきた',
   },
   {
-    name: 'Run',
-    github: 'https://github.com/Run51027 ',
+    name: 'Moom',
+    github: 'https://github.com/Moom51027 ',
     avatar: 'https://avatars.githubusercontent.com/u/168060328',
     headerImage: ume,
     role: 'Windowsアプリケーションの開発',
