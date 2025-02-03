8b8873143bd3b0ea5f7aed3bb36d64f50c7f61f0
Haru <23bus1maesub@gmail.com>
2025-02-03 11:29:24 +0900

    修正: インデントを修正

--- src/components/overlay/search/SearchButton.tsx
+++ src/components/overlay/search/SearchButton.tsx
@@ -3,7 +3,7 @@ import { toggleSearch }  from "../overlay";
 export function SearchButton() {
   return (
     <button onClick={toggleSearch} class= "btn-base size-otdm-btn">
-    <div class="i-tabler:search size-otdm-icon"></div>
-  </button>
+      <div class="i-tabler:search size-otdm-icon"></div>
+    </button>
   )
 }
