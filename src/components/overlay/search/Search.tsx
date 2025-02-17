926f8b78370e5865484fdd28ec7ebffb88192517
Haru <23bus1maesub@gmail.com>
2025-02-17 14:15:29 +0900

    修正: Astro.propsの型定義を追加

--- src/components/overlay/search/Search.tsx
+++ src/components/overlay/search/Search.tsx
@@ -3,8 +3,6 @@ import { SearchModal } from "./SearchModal";
 import { SearchButton } from './SearchButton';
 import { isSearchOpen } from "../overlay";
 
-
-
 export default function Search() {
   return (
     <search class={`flex items-center btn-search-style sm:shadow-md rounded-lg ${isSearchOpen() && "outline-(3 solid)"}`} >
